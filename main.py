cbb1459c3ffa9a1cff59331e042ee25d7a0bcac8
w3bFinger <mishka321123@gmail.com>
2024-03-08 08:00:10 +0200

    Added some more lines of code from another PC

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from settings import *
-
+from sys import exit
 class Main:
     def __init__(self):
 
         # general
         pygame.init()
         self.display_surface = pygame.display.set_mode((windows_width, window_height))
-
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Tetris")
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+
+                    #exit everything 
                     
             # display
+            self.display_surface.fill(gray)
+
             pygame.display.update()
+            self.clock.tick()
 
 if __name__ == "__main__":
     main = Main()
